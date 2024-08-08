1412a79aad311c9738cb62ec404bc637d0edc55f
le-pp2402 <lephiphatphat@gmail.com>
2024-08-08 18:49:15 +0700

    count friend

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,16 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class User {
+    private Integer id;
+    private String username;
+
+    public User(int id) {
+        this.id = id;
+        username = UUID.randomUUID().toString().replace("-", "");
+    }
 }
